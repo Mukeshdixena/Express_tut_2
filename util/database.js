f3e117e42ad1637bf13f180ebf03de60a21119a3
Mukesh <mukeshdixena123@gmail.com>
2025-02-01 14:29:03 +0530

    MySQL database is connected to node js server

---
+++ util/database.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'node-complete',
+    password: 'Chintu2@2002'
+
+})
+
+module.exports = pool.promise();
