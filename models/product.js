4a694bef42b1949962175dd22e5433e56549cd1f
Mukesh <mukeshdixena123@gmail.com>
2025-02-02 11:36:48 +0530

    Modified the server to use the sequelize

--- models/product.js
+++ models/product.js
@@ -1,51 +1,33 @@
-// const fs = require('fs');
-// const path = require('path');
-const db = require('../util/database');
-const Cart = require('./cart');
+const { Sequelize } = require('sequelize');
 
-// const p = path.join(
-//   path.dirname(process.mainModule.filename),
-//   'data',
-//   'products.json'
-// );
+const sequelize = require('../util/database');
 
-// const getProductsFromFile = cb => {
-//   fs.readFile(p, (err, fileContent) => {
-//     if (err) {
-//       cb([]);
-//     } else {
-//       cb(JSON.parse(fileContent));
-//     }
-//   });
-// };
-
-module.exports = class Product {
-  constructor(id, title, imageUrl, description, price) {
-    this.id = id;
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
+const Product = sequelize.define(
+  'product',
+  {
+    id: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+    },
+    title: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: Sequelize.DOUBLE,
+      allowNull: false,
+    },
+    imageUrl: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
   }
+);
 
-  save() {
-    return db.execute('INSERT INTO products (title, price, description, imageUrl) VALUES (?, ?, ?, ?)',
-      [this.title, this.price, this.description, this.imageUrl]
-    );
-
-  }
-
-  static deleteById(id) {
-
-    return db.execute('DELETE FROM products WHERE products.id = ?', [id]);
-  }
-
-  static fetchAll() {
-    return db.execute('SELECT * FROM products');
-  }
-
-  static findById(id) {
-
-    return db.execute('SELECT * FROM products WHERE products.id = ?', [id]);
-  }
-};
+module.exports = Product;
--- util/database.js
+++ util/database.js
@@ -1,11 +1,17 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: 'Chintu2@2002'
+const sequelize = new Sequelize('node-complete', 'root', 'Chintu2@2002', {
+    dialect: 'mysql',
+    host: 'localhost'
+});
 
-})
+sequelize.authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch((err) => {
+        console.error('Unable to connect to the database:', err);
+    });
 
-module.exports = pool.promise();
+module.exports = sequelize
+
